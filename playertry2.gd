b2b97c64999c6fd889b2d06f77557ab4399332df
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-04-28 19:39:03 -0400

    splits movement into air/floor

--- playertry2.gd
+++ playertry2.gd
@@ -7,8 +7,7 @@ const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -17,9 +16,16 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	handleMove(delta, input_dir)
+	handleMove(delta, input_dir) 
 	
 func handleMove(delta, inputs):
+	if not is_on_floor():
+		
+		handleair(delta, inputs)
+	else: 
+		handlefloor(delta, inputs)
+	
+func handlefloor(delta, inputs):
 	var direction := (transform.basis * Vector3(inputs.x, 0, inputs.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -27,6 +33,9 @@ func handleMove(delta, inputs):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	move_and_slide()
 
+func handleair(delta, inputs):
+	velocity += get_gravity() * delta
 	move_and_slide()
 	
