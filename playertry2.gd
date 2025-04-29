8f696775c44fcbdc83d0df473d6681452d300a48
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-04-29 13:48:31 -0400

    Gets things into a semiworking state

--- playertry2.gd
+++ playertry2.gd
@@ -5,12 +5,24 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const sourcelike = true
 
+#player camera variables
+var ylook : float
+var xlook : float
+# even more but constants
+var ply_mousesensitivity = 1.5
+var ply_maxlookangle_down = -90
+var ply_maxlookangle_up = 90
+var ply_ylookspeed = 0.3
+var ply_xlookspeed = 0.3
+
 #player input/movement variables
 const walkspeed = 20 #basic multiplier to walking
 const maxIn = 4096 #boundaries for the most "walking" we want to be doing
 const minIn = -4096
 var leftright : float #variables for how "walking" we are in either cardinal direction
 var forback : float
+var playerVelocity = Vector3.ZERO
+var onFloor = false
 
 const maxspeed = 32 # used in player velocity calculations as a clamp - handlefloorsourcelike
 const accelerateby = 1000 # used in dosourcelikeaccelerate
@@ -18,6 +30,25 @@ const accelerateby = 1000 # used in dosourcelikeaccelerate
 
 @onready var playerCam = $came
 @onready var colliderbottom = $bottom
+
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		InputMouse(event)
+		
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if event.is_action_pressed("click"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func InputMouse(event):
+	xlook += -event.relative.y * ply_xlookspeed 
+	ylook += -event.relative.x * ply_ylookspeed
+	
+	
+	xlook = clamp(xlook, ply_maxlookangle_down, ply_maxlookangle_up)
 
 # handle wasd inputs
 # will listen to keypresses and update the movement variables above
@@ -57,9 +88,9 @@ func _physics_process(delta: float) -> void:
 		handleMove(delta, input_dir) 
 	
 func handleMove(delta, inputs):
-	if not is_on_floor(): #air movement is the same
-		handleair(delta)
-	else: 
+	#if not is_on_floor(): #air movement is the same
+	#	handleair(delta)
+	#else: 
 		if sourcelike:
 			handleFloorSourcelike(delta)
 		else:
@@ -69,14 +100,16 @@ func handleMove(delta, inputs):
 func handleFloorSourcelike(delta):
 	#alter the forward movement by camera's azimuth rotation. shouldnt do anything yet.
 	var forwAngle = (Vector3.FORWARD).rotated(Vector3.UP, playerCam.rotation.y).normalized()
-	var sideAngle = (Vector3.LEFT).rotated(Vector3.UP, rotation.y).normalized()
+	var sideAngle = (Vector3.LEFT).rotated(Vector3.UP, playerCam.rotation.y).normalized()
 	
 	#calculate a vector based of our inputs and angles
 	var desiredVec = (leftright * sideAngle) + (forback * forwAngle)
+	
+	#deal with friction
+	handleFriction(delta)
+	
 	desiredVec.y = 0; #but no y.
 	
-		#haven't opened this can of worms yet
-	#friction(delta)
 	
 	
 	var desiredDir = desiredVec.normalized()
@@ -90,7 +123,7 @@ func handleFloorSourcelike(delta):
 	doSourceAccelerate(desiredDir, desiredSpeed, delta)
 	
 func doSourceAccelerate(desiredDir, desiredSpeed, delta):
-	var currentspeed = velocity.dot(desiredDir) # are we changing direction?
+	var currentspeed = playerVelocity.dot(desiredDir) # are we changing direction?
 	var addedspeed = desiredSpeed - currentspeed # reduce by amount
 	
 	if addedspeed <= 0: #no need to do anything
@@ -101,7 +134,7 @@ func doSourceAccelerate(desiredDir, desiredSpeed, delta):
 	acelspeed = min(acelspeed, addedspeed) #cap by addspeed
 	
 	for i in range(3): #the comment says adjust velocity but i truly have no idea what this does
-		velocity+= acelspeed * desiredDir
+		playerVelocity+= acelspeed * desiredDir
 	
 	#now that we've updated velocity, the godot function will take it from here
 	checkVelocityAndMove()
@@ -110,11 +143,11 @@ func doSourceAccelerate(desiredDir, desiredSpeed, delta):
 func handlefloorTemplate(delta, inputs):
 	var direction := (transform.basis * Vector3(inputs.x, 0, inputs.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		playerVelocity.x = direction.x * SPEED
+		playerVelocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		playerVelocity.x = move_toward(playerVelocity.x, 0, SPEED)
+		playerVelocity.z = move_toward(playerVelocity.z, 0, SPEED)
 	checkVelocityAndMove()
 
 func handleair(delta):
@@ -123,9 +156,73 @@ func handleair(delta):
 	
 func checkVelocityAndMove():
 	var maxvelocity = 35000; #why isn't this a constant declared higher? because you cant assign vectors to ints
-	if velocity.length() > maxvelocity: #how does declaring it down here do anything? genuinely couldnt tell you
-		velocity = maxvelocity
-	move_and_slide()
-
-
 	
+	
+	if playerVelocity.length() > maxvelocity: #how does declaring it down here do anything? genuinely couldnt tell you
+		playerVelocity = maxvelocity
+	
+	velocity = playerVelocity
+	move_and_slide()
+	playerVelocity = velocity
+
+func handleFriction(delta):
+	var speed = playerVelocity.length()
+	var friction = 4 # I know this should be a constant but I'm just trying to get things working at this point
+	var stopspeed = 100
+	var control = stopspeed if speed < stopspeed else speed
+	var drop = 0 + (control * friction * delta)
+	var newspeed = speed - drop
+	if newspeed < 0:
+		newspeed = 0
+	if newspeed != speed:
+		# Determine proportion of old speed we are using.
+		newspeed /= speed
+		# Adjust velocity according to proportion.
+		playerVelocity *= newspeed
+		
+func move_and_slide_sourcelike()->bool:
+	var collided := false
+
+	# Reset previously detected floor
+	#stats.on_floor  = false
+
+
+	#check floor
+	var checkMotion := velocity * (1/60.)
+	#checkMotion.y  -= stats.ply_gravity * (1/360.)
+		
+	var testcol := move_and_collide(checkMotion, true)
+
+	if testcol:
+		var testNormal = testcol.get_normal()
+		#if testNormal.angle_to(up_direction) < stats.ply_maxslopeangle:
+		#	stats.on_floor = true
+
+	# Loop performing the move
+	var motion := velocity * get_delta_time()
+	
+	for step in max_slides:
+		
+		
+		var collision := move_and_collide(motion)
+		
+		if not collision:
+			# No collision, so move has finished
+		
+			break
+
+		# Calculate velocity to slide along the surface
+	
+		var normal = collision.get_normal()
+		
+		motion = collision.get_remainder().slide(normal)
+		velocity = velocity.slide(normal)
+		# Collision has occurred
+		collided = true
+	return collided
+	
+func get_delta_time() -> float:
+	if Engine.is_in_physics_frame():
+		return get_physics_process_delta_time()
+
+	return get_process_delta_time()
