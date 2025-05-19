3e5fa3d810f3487d7cd76088c37d72b39e4e398d
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-05-19 14:23:46 -0400

    gets resource loading working

---
+++ scenes/Player associated/weaponload.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var WEP_TYPE: Wep
+@onready var weapon_mesh: MeshInstance3D = %DebugWeaponMesh
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	load_weapon()
+
+func load_weapon():
+	weapon_mesh.mesh = WEP_TYPE.mesh
+	position = WEP_TYPE.position
+	rotation_degrees = WEP_TYPE.rotation
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
