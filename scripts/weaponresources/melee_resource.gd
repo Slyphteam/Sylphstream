e90207a3e81f211002c8bcf91305602e3b2b2bf8
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-06-03 17:32:33 -0400

    updates some melee + imports npc dialogue

--- scripts/weaponresources/melee_resource.gd
+++ scripts/weaponresources/melee_resource.gd
@@ -9,11 +9,12 @@ class_name Mel extends Resource
 @export var rotation : Vector3 = Vector3(90, 90, 0)
 @export var scale : Vector3 = Vector3.ONE
 @export_category("Variables")
-@export var damage : int = 10
-@export var variance: float = 0.2
-@export var reach : float = 2
-@export var speed : int = 80
+@export var damage : int = 50 ##Should be fairly high to make melee viable
+@export var variance: float = 0.2 ##Fractional representation of how much damage can vary
+@export var reach : float = 1 ##In meters, from center of player
+@export var speed : int = 80 ##Swing cooldown, in frames
 @export_category("Behaviors")
-@export var canThrust : bool = true
-@export var canParry : bool = true
-@export var effectiveParry : bool = true 
+@export var canThrust : bool = true ##1.5x reach charge attack
+@export var canParry : bool = true ##Block behavior with partial melee damage reduction
+@export var effectiveParry : bool = true ##Block behavior with full melee damage reduction
+#
