8246d346e2445d854497e54c6d8ff177d577bc64
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-06-16 22:18:47 -0400

    adjusts offsets for rescaled firearms

--- scripts/weapon_parent.gd
+++ scripts/weapon_parent.gd
@@ -7,6 +7,6 @@ class_name WEAPON_PARENT extends Resource
 @export var secondMesh: Mesh ##Secondary model. Sheathe or magazine.
 @export var damage: int = 1 
 @export var selection : int = 4 ##Currently unused, will be for switching
-@export var position : Vector3 = Vector3(0, -0.3, -0.3) ##Offset from camera center
+@export var position : Vector3 = Vector3(0, -0.3, -0.3) ##Offset from camera center in L/R, U/D, F/B
 @export var rotation : Vector3 = Vector3(90, 90, 0) 
 @export var scale : Vector3 = Vector3.ONE ##In case model is incorrectly sized
