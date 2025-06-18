eccb66c7bbf7919e5cefc990bb42a25fefbd9006
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-06-17 20:46:42 -0400

    cleans textures up and adds 9mm, rifle pickup boxes

--- scripts/trivial/pickups/9mmbox_world_script.gd
+++ scripts/trivial/pickups/9mmbox_world_script.gd
@@ -4,7 +4,6 @@ func interact_By_Player(player):
 	var invem: INVENMANAGER = player.invenManager
 	#give 50 rounds of 9mm, the standard amoutn in a "small" box. (sh/c)ould be reduced for balance
 	invem.giveAmmo(1, 50) 
-	print("bye")
 	
 	var boxOrigin = $"../.."
 	boxOrigin.queue_free() #delete not only self, but also the full ammobox scene tree
---
+++ scripts/trivial/pickups/rifle_ammobox_worldscript.gd
@@ -0,0 +1,9 @@
+extends raycast_reactive
+
+func interact_By_Player(player):
+	var invem: INVENMANAGER = player.invenManager
+	#Give 20 rifle rounds. a little on the low side but hey that's what small boxes have in them
+	invem.giveAmmo(2, 20) 
+	
+	var boxOrigin = $"../.."
+	boxOrigin.queue_free() #delete full tree
