8d4f3fa6e71fa0f5675ffee78e6da1778364ffc6
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-06-24 15:11:21 -0400

    Fixes file loading

--- scripts/archonstript.gd
+++ scripts/archonstript.gd
@@ -2,23 +2,18 @@ class_name ARCHONHELPER extends Node3D
 
 func _process(delta):
 	do_Archon_Gaylittlefloat(delta)
-	
 
-var bobAmplitude = 200
 var freq = 2
 var danceAmplitude = 4
 var calcdSin : float
 
 func do_Archon_Gaylittlefloat(delta):
 	calcdSin = sin(Globalscript.timer * freq)
-	#print(calcdSin)
-	#self.position.y += cos(Globalscript.timer) / bobAmplitude #don't like this
-	
 	var tilt = calcdSin * danceAmplitude
 	self.rotation.z = deg_to_rad(tilt)
 
 func get_Talked_To(player:Node3D):
-	print("Hi Millian! What can I do for you today?")
+	#print("Hi Millian! What can I do for you today?")
 	look_At_Player(player)
 	file_Load_Test()
 	
@@ -46,13 +41,10 @@ func look_At_Player(player:Node3D):
 #https://docs.godotengine.org/en/stable/tutorials/io/runtime_file_loading_and_saving.html
 # https://kidscancode.org/godot_recipes/4.x/basics/file_io/index.html
 func file_Load_Test():
-	
-	var ourFile: FileAccess = FileAccess.open("res://resources/txt files/examplefile.txt", FileAccess.WRITE)
+	var ourFile: FileAccess = FileAccess.open("res://resources/txt files/examplefile.txt", FileAccess.READ)
 	var line1 : String = ourFile.get_line()
+	var line2 : String = ourFile.get_line()
 	print(line1)
-	#var line2 : String = ourFile.get_line()
-	#print(line2)
-	#var line3 : String =
-	#var line4 : String =
+	print(line2)
 	ourFile.close()
 	
