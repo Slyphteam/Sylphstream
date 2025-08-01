7940b4ad9e819ce1dce5d58dc97f60656174c4ce
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-08-01 18:57:08 -0400

    oops, other firearm scripts didnt update

--- scripts/mob/complex/player/ui associated/ui_Manager.gd
+++ scripts/mob/complex/player/ui associated/ui_Manager.gd
@@ -1,5 +1,35 @@
 ##Fairly trivial script that acts as a librarian for all the different UI elements. 
-extends Node
+extends Control
 @onready var theReticle = $Reticle
 @onready var ammoCounter = $Ammo
 @onready var pauseMenu = $pauseContainer
+
+
+##reticle stuff
+func _ready() -> void:
+	queue_redraw()
+
+var dot_rad: float = 2
+var dot_col: Color = Color.BLACK
+var centerdot_rad: float = 1.0
+var centerdot_col: Color = Color.WHITE
+
+#dynamic reticle
+@export var crosshairs : Array[Line2D]
+@export var ourplayer : CharacterBody3D
+@export var reticlespeed : float = 0.25
+@export var reticledistance : float = 2
+
+func _draw():
+	draw_circle(Vector2(0,0), dot_rad, dot_col)
+	draw_circle(Vector2(0,0), centerdot_rad, centerdot_col)
+
+#for movement speed: lerp crosshairs[0].position, [0,0] + vector 2 of 0, -playerspeed  * reticledistance
+#adjust the spread of the crosshairs by a given amount. input MUST be an integer.
+func adjust_spread(amount):
+	
+	crosshairs[0].position = Vector2(0, -amount) 
+	crosshairs[1].position = Vector2(0, amount)
+	crosshairs[2].position = Vector2(-amount, 0)
+	crosshairs[3].position = Vector2(amount, 0)
+	pass
