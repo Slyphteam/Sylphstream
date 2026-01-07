946849cbf74b65b1298cb7a9f977cc9dc47c4b08
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2026-01-07 15:33:59 -0500

    Organization prep for Springtails

--- scripts/trivial/dev and debug/shapeButtonScript.gd
+++ scripts/trivial/dev and debug/shapeButtonScript.gd
@@ -2,4 +2,8 @@
 extends StaticBody3D
 @export var objectType:int = 1
 func interact_By_Player(player):
+	if(objectType == 3):
+		print("Blue sphere")
+	if(objectType == 2):
+		print("Red cone")
 	print("you just pressed the shape button")
--- scripts/trivial/mystery_button_behav.gd
+++ scripts/trivial/mystery_button_behav.gd
@@ -1,8 +1,10 @@
 ##Simple script that harms the player by shooting them when a button is pressed.
 extends StaticBody3D
-
+@export var springtail: Node3D
 ##Creates a bullet that shoots the player.
 func interact_By_Player(player):
-	#print("Wow! You just pressed a MYSTERY BUTTON!")
+	print("Wow! You just pressed a MYSTERY BUTTON!")
+	
 	#print("Randf value ", randf_range(0-testy, testy))
-	print("Better randf value ", Globalscript.better_Randf_Simple(1, 2, 1))
+	#print("Better randf value ", Globalscript.better_Randf_Simple(1, 2, 1))
+	
