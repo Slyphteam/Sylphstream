d2507e54b9a0555fcbc930f452852d43365541cc
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2026-01-09 14:29:22 -0500

    learns animation system, imports springtails, rearranges environ scene

--- scripts/mob/simple/enemy/springtailScript.gd
+++ scripts/mob/simple/enemy/springtailScript.gd
@@ -1,1 +1,9 @@
 extends CharacterBody3D
+
+@onready var ourModel = $Skeleton3D/springtail_001
+#
+#func _ready() -> void:
+	#var testy = ourModel.get_surface_override_material(0)
+	#testy.albedo_color = Color.DARK_TURQUOISE
+	#testy.albedo_color.a = 0.5
+	##().albedo = Color.DARK_CYAN
